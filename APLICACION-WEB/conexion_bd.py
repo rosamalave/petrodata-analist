6a4f6de99451796d8b1c1b90ce14cdac9c83b7cd
rosamalave <rosamalavework@gmail.com>
2024-11-18 16:03:57 -0400

    funciones bd +ui: insertar, editar, eliminar. Sin conectar a botones. Sin probar

--- APLICACION-WEB/conexion_bd.py
+++ APLICACION-WEB/conexion_bd.py
@@ -2,22 +2,81 @@ import psycopg2
 from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
 from datetime import datetime
 from decimal import Decimal
+import sys
 
 # Conexión a PostgreSQL
-class base_ddatos():
 
+class base_ddatos():
 
     def __init__(self):
 
         #objeto de tipo conexion que guarda metodos: cursor, commit, rollback, close
         self.conn = psycopg2.connect(host="localhost", database="PETROJUNIN_DATA", user="postgres", password="Junindata" )
 
+    def insertar(self,producto,nuevaf,esquema,tabla):
+        cursor=self.conn.cursor()
+        self.conversionformatotabla(cursor, nuevaf,esquema,tabla)
+    
+        #extrayendo headers en una cadena
+        nombres=[]
+        headers=self.header(cursor,nombres,esquema,tabla)
+                
+        #extrayendo datos en una cadena
+        #def agregar datos/editar validar nuevaf
+        datos=""
+        for col in nuevaf:
+            datos=datos+col+", "
+        if datos.endswith(", "):
+            datos = datos[:-2]
 
-    def conversionformatotabla(self, cursor, nuevaf, nesquema, ntabla):
-        #v vector a insertar
-        #bdtype tupla con tipos de datos 
+        cursor.execute(f"INSERT INTO {esquema}.{tabla}({headers}) VALUES({datos})")
+        self.conn.commit()
+        cursor.close()
 
-        cursor.execute(f"SELECT data_type FROM information_schema.columns WHERE table_schema = '{nesquema}' AND table_name = '{ntabla}' AND column_name NOT LIKE 'id%'")
+        #usada para editar e insertar informacion de 0 (puede ser la misma funcion)
+        
+    def eliminar(self,esquema,tabla):
+        cursor=self.conn.cursor()
+        id=self.buscar
+        if id != -1:
+            cursor.execute(f"DELETE * FROM {esquema}.{tabla} WHERE id_{tabla} = {id}")
+        self.conn.commit()
+        cursor.close()
+        
+    def editar (self,producto,nuevaf,esquema,tabla):
+        #sirve recibiendo de nuevo toda la fila completa y cambiando todos los campos
+        #-incluso los no editados
+        cursor=self.conn.cursor()
+        self.conversionformatotabla(cursor, nuevaf,esquema,tabla)
+
+        #header en vector
+        nombres=[]
+        #extrayendo header en una cadena
+        headers=self.header(cursor,nombres,esquema,tabla)
+        #extrayendo datos en una cadena
+        datos=""
+        i=0
+        for col in nuevaf:
+            if "id" not in col:
+                datos=datos+nombres[i]+'= "'+col+'", '
+            i=i+1
+        if datos.endswith(", "):
+            datos = datos[:-2]
+
+        cursor.execute(f"UPDATE {esquema}.{tabla} SET {datos} WHERE id_{tabla} = {id}")
+        self.conn.commit()
+        cursor.close()
+
+    def buscar(self,cursor,id,esquema,tabla):
+        cursor.execute(f"SELECT * FROM {esquema}.{tabla} WHERE id_{tabla} = {id}")
+        if cursor.rowcount == 0 or cursor.rowcount ==-1:
+            print("fila no existe.")
+            return -1
+        else:
+            return id
+        
+    def conversionformatotabla(self, cursor, nuevaf, esquema, tabla):
+        cursor.execute(f"SELECT data_type FROM information_schema.columns WHERE table_schema = '{esquema}' AND table_name = '{tabla}' AND column_name NOT LIKE 'id%'")
         bdtype= cursor.fetchall()
 
         for i in range (len(nuevaf)):
@@ -28,20 +87,24 @@ class base_ddatos():
             elif bdtype(i)=='numeric':
                 nuevaf[i]=Decimal(nuevaf[i])
 
+        #nocierracursor
+        #v vector a insertar
+        #bdtype tupla con tipos de datos       
 
+    def header(self,cursor,nombres,esquema,tabla):
+        cursor.execute(f"SELECT column_name FROM information_schema.columns WHERE table_schema = '{esquema}' AND table_name = '{tabla}' AND column_name NOT LIKE 'id%'")
 
-    def insertar(self,producto,nuevaf,nesquema,ntabla):
-        cursor=self.conn.cursor()
-        self.conversionformatotabla(cursor, nuevaf,nesquema,ntabla)
-        cursor.execute("INSERT INTO public.producto(id_producto)VALUES (%s)", producto)
-        cursor.execute("INSERT INTO public.produccion_c(fecha, diluente, bpd) VALUES(%s, %s, %s)", (nuevaf[0], nuevaf[1], nuevaf[2]))
-        self.conn.commit()
+        #extrayendo headers en un vector
+        for col in cursor.fetchall():
+            nombres.append(str(col[0]))
 
+        #extrayendo headers en una cadena
+        cadenanombres=""
+        for col in nombres:
+            cadenanombres=cadenanombres+col+", "
+
+        return cadenanombres
     
-
-#produccionc 3 
-#potencial 6
-#producciong 2
-#producciondiferida 3
-#diferida np 28
-#diferida p 8
+        #nocierra cursor
+        #modifica vector nombres
+        #retorna en un string el vector nombres
--- APLICACION-WEB/main.py
+++ APLICACION-WEB/main.py
@@ -2,9 +2,9 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QTableWidget, QTableWidgetItem
 from primera_tabla import Ui_Form
 from conexion_bd import base_ddatos
-
+#esquema= "public" o "public2" | tabla= "produccion_c"
 class MainApp(QWidget):
-    def __init__(self, tabla,ctabla):
+    def __init__(self, esquema,tabla):
         super().__init__()
         self.ui = Ui_Form()
         self.ui.setupUi(self)
@@ -13,22 +13,21 @@ class MainApp(QWidget):
         self.conectarbd = base_ddatos()
 
         # Inicializar la tabla en la interfaz con los datos desde la base de datos
-        self.inicializar_tabla(tabla,ctabla)  # Pasa el nombre de la tabla a inicializar
+        self.inicializar_tabla(esquema,tabla)  # Pasa el nombre de la tabla a inicializar
 
         # Conectar el botón "aggfila" al método agregar_fila
         self.ui.aggfila.clicked.connect(self.agregar_fila)
 
-    def inicializar_tabla(self, tabla,ctabla):
+    def inicializar_tabla(self, esquema,tabla):
         # Crear un cursor
         cursor = self.conectarbd.conn.cursor()
 
-        # Ejecutar consulta para obtener datos de la tabla
-        cursor.execute(f"SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name = '{tabla}' AND column_name NOT LIKE 'id%'")
+        # Ejecutar consulta para obtener header de la tabla
         nombres = []
-        for col in cursor.fetchall():
-            nombres.append(str(col[0]))
- 
-        cursor.execute(f"SELECT * FROM {ctabla}")
+        self.conectarbd.header(cursor,nombres,esquema,tabla)
+        
+        # Ejecutar consulta para obtener datos de la tabla
+        cursor.execute(f"SELECT * FROM {esquema}.{tabla}")
         rows = cursor.fetchall()  # Obtiene todas las filas
 
         # Configurar la tabla en la interfaz
@@ -39,15 +38,33 @@ class MainApp(QWidget):
             for j, col in enumerate(row):
                 self.ui.tabla.setItem(i, j, QTableWidgetItem(str(col)))  # Agrega los datos a la tabla
         
-        cursor.close()
-        
         self.ui.tabla.setColumnHidden(0, True)  # Oculta la primera columna
         self.ui.tabla.setColumnHidden(1, True)  # Oculta la segunda columna
         #asignando encabezados a la tabla
         for col in range(len(nombres)):
             self.ui.tabla.setHorizontalHeaderItem(col+2, QTableWidgetItem(nombres[col]))
         
-        
+        cursor.close()
+
+    def agregareditar(self, producto,esquema,tabla):
+        #falta modulo de validacion
+
+        #extrayendo datos de la fila editada
+        filaseleccionada = self.ui.tabla.currentRow()
+        if filaseleccionada != -1:  # Si hay una fila seleccionada
+            nuevaf = []
+            for col in range(self.table.columnCount()):
+                item = self.table.item(filaseleccionada, col)
+                if item:
+                    nuevaf.append(item.text())  # Guardar el texto de la celda
+            print(f"Fila seleccionada:{nuevaf}")  # Mostrar la fila en la consola (opcional)
+
+        if None == self.posicionfila():
+            self.conectarbd.insertar(producto,nuevaf,esquema,tabla)
+        else:
+            self.conectarbd.editar(producto,nuevaf,esquema,tabla)
+
+
     def agregar_fila(self):
         # Obtiene el número de filas actuales y añade una nueva al final
         nfilas = self.ui.tabla.rowCount()
@@ -58,9 +75,26 @@ class MainApp(QWidget):
             item = QTableWidgetItem("")  # Crea un item vacío
             self.ui.tabla.setItem(nfilas, col, item)  # Añade el item vacío a la nueva fila
 
+    def posicionfila(self):
+        # Obtener el índice de la fila seleccionada
+        filaseleccionada = self.ui.tabla.currentRow()
+
+        if filaseleccionada != -1:  # Verifica que haya una fila seleccionada
+            #item(variable,posicion fila)
+            item_id = self.item(filaseleccionada, 1)  # Columna 1 contiene el 'id'
+            if item_id:  # Verifica que el item no sea None
+                return int(item_id.text())  # Devuelve el id como entero
+        return None  # Si no hay fila seleccionada, devuelve None
+
+    
+
+
+#def agregar datos/editar que retorne una cadena de  texto de una vez
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     # Inicia el MainApp y especifica el nombre de la tabla que deseas cargar al inicio
-    mainApp = MainApp("produccion_c","public.produccion_c")
+    mainApp = MainApp("public","produccion_c")
     mainApp.show()
     sys.exit(app.exec_())
